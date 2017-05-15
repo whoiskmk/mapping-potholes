6d8a85c7877b61f2309e4335a3623ac736a3853a
whoiskmk <kellykrawczyk@mac.com>
2017-05-15 11:12:22 -0500

    Final

--- maps/testing.js
+++ maps/testing.js
@@ -1,10 +1,4 @@
-/**
- * Created by kellykrawczyk on 4/11/17.
- */
-
-
-
-var map, L, MQ, myURL, myIcon, cpts;
+var map, L, MQ, cpts, potholeCluster, rl, idx = 0;
 
 function init() {
 
@@ -19,145 +13,150 @@ function init() {
     L.control.scale().addTo(map);
 
 
+
     cpts = new L.layerGroup();
     cpts.addTo(map);
 
-
-
-    var potholes = new L.markerClusterGroup({
-        maxClusterRadius: 40
+    potholeCluster = new L.markerClusterGroup({
+        maxClusterRadius: 30,
+        removeOutsideVisibleBounds: true
     });
     for (var i = 0; i < markers.length; i++) {
         var latlng = new L.LatLng(markers[i].lat, markers[i].lng);
-        potholes.addLayer(new L.Circle(latlng, 10));
-    }
-    map.addLayer(potholes);
 
+        var potholeMarker = new L.Circle(latlng, 10,
+            {
+                id: idx,
+                cweight: 100,
+                color: 'red',
+                fillColor: 'red',
+                opacity: 1,
+                fillOpacity: 0.5
+            }
+        ).addTo(potholeCluster);
+        potholeCluster.addTo(cpts);
 
-    /*myURL = jQuery('script[src$="testing.js"]').attr('src').replace('testing.js', '');
+        //map.addLayer(potholes);
+        //var visibleOne = potholes.getVisibleParent(i);
 
-    myIcon = L.icon({
-        iconUrl: myURL + 'images/pin24.png',
-        iconRetinaUrl: myURL + 'images/pin48.png',
-        iconSize: [29, 24],
-        iconAnchor: [9, 21]
-    });
-
-    for (let i = 0; i < markers.length; ++i) {
-        let m = L.marker(
-            [markers[i].lat, markers[i].lng],
-            {icon: myIcon}
-            ).addTo(potholes);
-        potholes.addLayer(m);
+        //idx = idx + 1;
     }
 
-    //map.addLayer(MQ.mapLayer());
-    //map.addLayer(cpts);
-*/
+
+
 }
 window.onload = init;
-/*function routeit() {
 
-    var dir = MQ.routing.directions();
+function routeit() {
+
+    var cpc = [], dir = MQ.routing.directions()
+        .on('success', function(data){
+            var legs = data.route.legs,
+                html = '',
+                maneuvers,
+                i;
+
+            if (legs && legs.length) {
+                maneuvers = legs[0].maneuvers;
+
+                for (i = 0; i < maneuvers.length; i++) {
+
+                    html += (i+1) + '. ';
+                    html += maneuvers[i].narrative + '';
+                }
+                L.DomUtil.get('route-narrative').innerHTML = html;
+            }
+
+        });
+
+
+
+    cpts.eachLayer(function(layer){
+
+        if (layer.getChildCount > 20) {
+            cpc.push({
+                lat: layer.lat,
+                lng: layer.lng,
+                weight: 100,
+                radius: 1
+            });
+        }
+        if (layer.getChildCount < 20 && layer.getChildCount > 10) {
+            cpc.push({
+                lat: layer.lat,
+                lng: layer.lng,
+                weight: 50,
+                radius: 0.75
+            });
+        }
+        if (layer.getChildCount < 10 && layer.getChildCount > 5) {
+            cpc.push({
+                lat: layer.lat,
+                lng: layer.lng,
+                weight: 25,
+                radius: 0.1
+            });
+        }
+        if (layer.getChildCount < 5 && layer.getChildCount > 2) {
+            cpc.push({
+                lat: layer.lat,
+                lng: layer.lng,
+                weight: 10,
+                radius: 0.1
+            });
+        }
+        if (layer.getChildCount < 2) {
+            cpc.push({
+                lat: layer.lat,
+                lng: layer.lng,
+                weight: 0.9,
+                radius: 0.1
+            });
+        }
+
+
+
+    });
+
+    /* cpts.eachLayer(function (d) {
+
+     var w = 0;
+
+     if (d.options.fillColor === 'red') {
+
+     w = d.options.fillOpacity * 200;
+     } else {
+     w = 1 - (d.options.fillOpacity * 2)
+     }
+     cpc.push({
+     lat: d.lat,
+     lng: d.lng,
+     weight: w,
+     radius: 0.1
+     });
+     });*/
+
 
     dir.route({
-
         locations: [
-            'new orleans LA',
-            'metarie LA'
-        ]
+            document.getElementById('orig').value,
+            document.getElementById('dest').value
+        ],
+        options: {
+            routeControlPointCollection: cpc
+        }
     });
 
-
-    map.addLayer(MQ.routing.routeLayer({
-        directions: dir,
-        fitBounds: true
-
-    }));
-}
-
-*/
-
-
-/*
-
-var L, MQ, map, idx = 0, cps, rl;
-
-function init() {
-    map = L.map('map', {
-        layers: MQ.mapLayer(),
-        center: [29.951065, -90.071533],
-        zoom: 15
-    });
-
-    L.control.scale().addTo(map);
-
-    cps = new L.featureGroup();
-    cps.addTo(map);
-
-
-}
-
-*/
-
-
-/*
-var tiles = L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    subdomains: ['a','b','c']
-}),
-latlng = L.latLng(29.951065, -90.071533);
-
-var map = L.map('map', {center: latlng, zoom: 15, layers: [tiles]});
-
-var potholes = L.markerClusterGroup({ spiderfyOnMaxZoom: false, showCoverageOnHover: false, zoomToBoundsOnClick: false });
-
-function populate() {
-    var myURL = jQuery( 'script[src$="testing.js"]' ).attr( 'src' ).replace( 'testing.js', '' );
-    var myIcon = L.icon({
-        iconUrl: myURL + 'images/pin24.png',
-        iconRetinaUrl: myURL + 'images/pin48.png',
-        iconSize: [29, 24],
-        iconAnchor: [9, 21]
-    });
-    for ( var i = 0; i < markers.length; ++i ) {
-        var m = L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} );
-        markerClusters.addLayer( m );
+    if (rl) {
+        map.removeLayer(rl);
     }
-    return false;
+
+    rl = MQ.routing.routeLayer({
+        directions: dir,
+        fitBounds: true,
+        ribbonOptions: {draggable: false}
+    });
+    rl.addTo(map);
+
+    map.addLayer(rl);
 }
-
-potholes.on('clusterclick', function(a) {
-    a.layer.spiderfy();
-});
-
-populate();
-map.addLayer(potholes);
-
-*/
-/*TEST OF MARKER CLUSTER DOCUMENTATION FROM GITHUB ABOVE */
-
-
-
-/*
-L.control.scale().addTo(map);
-
-cps = new L.featureGroup();
-cps.addTo(map);
-
-var myURL = jQuery( 'script[src$="leaf-demo.js"]' ).attr( 'src' ).replace( 'leaf-demo.js', '' );
-
-var myIcon = L.icon({
-    iconUrl: myURL + 'images/pin24.png',
-    iconRetinaUrl: myURL + 'images/pin48.png',
-    iconSize: [29, 24],
-    iconAnchor: [9, 21]
-});
-
-var markerClusters = L.markerClusterGroup();
-
-
-
-map.addLayer( markerClusters );*/
-
-
